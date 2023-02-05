d8398acb8d40a033dd054066916580dc6325afb0
ChipchikCat <kuba.z.chipchik@gmail.com>
2023-02-05 15:32:31 +0300

    commit12

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector(".popup");
 const openBtn = document.querySelector(".banner__list__button-js")
 
-openBtn.addEventListener('click', function() {
+openBtn.addEventListener('click', function () {
     popup.classList.add('open')
 })
 
